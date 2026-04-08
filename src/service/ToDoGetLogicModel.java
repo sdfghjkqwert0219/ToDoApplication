79f090397b9f3263a9db7bbdf5d6b5a96cfdae48
sdfghjkqwert0219 <hw.tpa.kunrenyou071219@gmail.com>
2026-04-08 17:48:27 +0900

    空にしました

--- src/service/ToDoGetLogicModel.java
+++ src/service/ToDoGetLogicModel.java
@@ -1,60 +1,7 @@
 package service;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalDate;
-
-import model.ToDoListModel;
-import model.ToDoModel;
-
 //ToDo詳細取得
 public class ToDoGetLogicModel {
 
-	
-	//ToDo詳細読み込み
-	public void getTaskDetails(ToDoListModel lm,int previewNo) {
 		
-		// 読み込むファイルパス
-		Path path = Paths.get(lm.getFolder() +"\\" + lm.getFileNames()[previewNo]);
-		
-		 // UTF-8で全行読み込み（List<String>として取得）
-		try {
-			lm.setLines(Files.readAllLines(path, StandardCharsets.UTF_8))  ;
-        
-        } catch (IOException e) {
-            System.err.println("ファイル読み込みエラー: " + e.getMessage());
-        }
-    }
-		
-	
-	//内容格納
-	public void roadingDataStorage(ToDoModel m,ToDoListModel lm) {
-				
-		//ToDo番号
-		m.setNo(Integer.parseInt(lm.getLines().get(0).replace("No.", "")));
-		
-		//タスク内容
-		m.setTask(lm.getLines().get(1).replace("タスク：", ""));
-		
-		//ステータス
-		m.setStatus(Boolean.parseBoolean(lm.getLines().get(2).replace("ステータス：", "")));
-		
-		//期日
-		String date = lm.getLines().get(3).replace("期日：", "");
-		
-		int year = Integer.parseInt(date.substring(0, 4));
-		int month = Integer.parseInt(date.substring(6, 7));
-		int day = Integer.parseInt(date.substring(9, 10));
-		
-		m.setDay(LocalDate.of(year, month, day));
-		
-		//メモ
-		m.setMemo(lm.getLines().get(4).replace("メモ：", ""));
-		
-		
-	}
-	
 }
--- src/view/MessageView.java
+++ src/view/MessageView.java
@@ -3,39 +3,4 @@ package view;
 public class MessageView{
 
 	
-	public void goHomeMsg() {	
-		
-		System.out.println("\nトップページに戻ります\n");
-	}
-	
-	public void addCmpMsg() {	
-		
-		System.out.println("登録完了しました\n");
-	}
-
-	public void chngCmpMsg() {	
-		
-		System.out.println("変更完了しました\n");
-	}
-
-	public void stsCmpMsg() {	
-		
-		System.out.println("お疲れ様でした\n");
-	}
-
-	public void delCmpMsg() {	
-		
-		System.out.println("削除完了しました\n");
-	}
-
-	public void endMsg() {	
-		
-		System.out.println("\n処理を終了します");
-	}
-
-	public void errorMsg() {	
-		
-		System.out.println("\n処理エラーが発生しました");
-	}
-
 }
